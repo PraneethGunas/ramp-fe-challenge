32e458f64d73ab14c079f20cee7028098171807e
PraneethGunas <praneethgunas404@gmail.com>
2025-02-27 16:20:48 -0500

    Fix: Bug 3 - Update onChange handler in App component to handle null and empty ID cases

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,13 @@ export function App() {
           })}
           onChange={async (newValue) => {
             if (newValue === null) {
-              return
+               return
+            } else if (newValue.id === "") {
+              await loadAllTransactions()
+            } else {
+              await loadTransactionsByEmployee(newValue.id)
             }
 
-            await loadTransactionsByEmployee(newValue.id)
           }}
         />
 
