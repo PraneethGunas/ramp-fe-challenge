7ff1b50e2b8181812e1703de4f9fb362cb9e9f42
PraneethGunas <praneethgunas404@gmail.com>
2025-02-27 16:40:13 -0500

    Fix: Bug 7 - Update button disabled state to account for nextPage availability in paginated transactions

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ export function App() {
           {transactions !== null && (
             <button
               className="RampButton"
-              disabled={paginatedTransactionsUtils.loading}
+              disabled={paginatedTransactionsUtils.loading || !paginatedTransactions?.nextPage }
               onClick={async () => {
                 await loadAllTransactions()
               }}
